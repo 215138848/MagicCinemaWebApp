779e32a90ea238a1e581129485525454b7df0c84
Rick Allen Herbert <219014337@mycput.ac.za>
2021-08-28 07:14:23 +0200

    Project update including navigation bar testing

---
+++ magic-cinema-app/src/app/employee.ts
@@ -0,0 +1,11 @@
+export interface Employee {
+    employeeNumber : string;
+    name : string;
+    surname : string;
+    email : string;
+    dateCreated : string
+    password : number;
+    role : string;
+    imagePath : string;
+
+}
